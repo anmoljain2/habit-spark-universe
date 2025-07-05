18044630c4d1644a71981b7a8e2d5824f0ac2394
Anmol Jain <anmoljain@berkeley.edu>
2025-07-05 11:34:15 -0700

    fixed pages maybe

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 import News from './pages/News'
+import { AuthProvider } from './hooks/useAuth'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
